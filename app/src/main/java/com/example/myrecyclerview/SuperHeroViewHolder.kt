3f4be4087cb5d50ace1daa502b5dedfc423fa4c1
Antonio <antoniogallegos086@gmail.com>
2023-02-10 15:17:56 -0600

    Lambda function

--- app/src/main/java/com/example/myrecyclerview/SuperHeroViewHolder.kt
+++ app/src/main/java/com/example/myrecyclerview/SuperHeroViewHolder.kt
@@ -3,22 +3,27 @@ package com.example.myrecyclerview
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.myrecyclerview.databinding.ItemSuperheroBinding
 
 class SuperHeroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    //Haciendo referencia s la vista inflada del item_superhero.xml
+    val binding = ItemSuperheroBinding.bind(view)
+
     val superHero = view.findViewById<TextView>(R.id.tvSuperHeroName)
     val realName = view.findViewById<TextView>(R.id.tvrealSuperName)
     val publisher = view.findViewById<TextView>(R.id.tvPublisher)
     val photo = view.findViewById<ImageView>(R.id.ivSuperHero)
 
     fun render(superHeroModel: SuperHero) {
-        superHero.text = superHeroModel.superHero
-        realName.text = superHeroModel.realName
-        publisher.text = superHeroModel.publisher
-
-        Glide.with(photo.context).load(superHeroModel.photo).into(photo)
-
-
+        binding.tvSuperHeroName.text = superHeroModel.superHero
+        binding.tvrealSuperName.text = superHeroModel.realName
+        binding.tvPublisher.text = superHeroModel.publisher
+        Glide.with(binding.ivSuperHero.context).load(superHeroModel.photo).into(binding.ivSuperHero)
+        binding.ivSuperHero.setOnClickListener {
+            Toast.makeText(binding.ivSuperHero.context,superHeroModel.realName,Toast.LENGTH_SHORT).show()
+        }
     }
 }
